7aa5b4bc99f020a99e77170e2e3338e267fc5872
lucan <lucan.chen@zrdinvestment.com>
2016-12-19 13:54:09 +0800

    core: add market interface

---
+++ core/market/fwd.h
@@ -0,0 +1,14 @@
+#ifndef FH_CORE_MARKET_FWD_H
+#define FH_CORE_MARKET_FWD_H
+
+namespace fh {
+namespace core {
+namespace market {
+
+class MarketI;
+class MarketListenerI;
+
+} // market
+} // core
+} // fh
+#endif  // FH_CORE_MARKET_FWD_H
---
+++ core/market/marketi.h
@@ -0,0 +1,31 @@
+#ifndef FH_CORE_MARKET_MARKETI_H
+#define FH_CORE_MARKET_MARKETI_H
+
+#include "core/market/fwd.h"
+
+#include <vector>
+
+namespace fh {
+namespace core {
+namespace market {
+
+typedef std::vector<std::string> InstrumentVec;
+
+class MarketI {
+public:
+  MarketI(core::market::MarketListenerI *listener) {};
+  virtual ~MarketI() {};
+
+  virtual void Initialize(InstrumentVec insts) = 0;
+  virtual bool Start() = 0;
+  virtual void Stop()  = 0;
+
+  virtual void Subscribe(InstrumentVec instruments)       = 0;
+  virtual void UnSubscribe(InstrumentVec instruments)     = 0;
+  virtual void ReqDefinitions(InstrumentVec instruments)  = 0;
+};
+
+} // market
+} // core
+} // fh
+#endif // FH_CORE_MARKET_MARKETI_H
