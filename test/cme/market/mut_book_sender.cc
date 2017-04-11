93420646a72b7f4ad996d1d96cf50720246906f2
sailing109 <461010295@qq.com>
2017-04-11 09:57:38 +0800

    add mut_book_sender.cc case

--- test/cme/market/mut_book_sender.cc
+++ test/cme/market/mut_book_sender.cc
@@ -43,7 +43,7 @@ namespace market
         OnMarketDisconnect_Test001();
     }
     
-    // BookSender::OnContractDefinition case
+    // BookSender::OnContractDefinition case, desc:check const parameter after it is called 
     TEST_F(MutBookSender, OnContractDefinition_Test001) 
     {
         BookSender *book_sender = nullptr;
@@ -52,8 +52,28 @@ namespace market
         {
             pb::dms::Contract contract;
             fh::cme::market::message::Instrument instrument;
-            contract.set_name(instrument.symbol);            
+            contract.set_name(instrument.symbol);
+            auto contractTypeValue = ::pb::dms::ContractType::CT_Futures;
+            contract.set_contract_type(contractTypeValue);
             book_sender->OnContractDefinition(contract);
+            delete book_sender;
+            book_sender = nullptr;
+            ASSERT_EQ(contract.contract_type(), ::pb::dms::ContractType::CT_Futures);
+        }
+    }
+    
+    // BookSender::OnBBO case
+    TEST_F(MutBookSender, OnBBO_Test001) 
+    {
+        BookSender *book_sender = nullptr;
+        book_sender = new BookSender("tcp://*:5557", "tcp://*:5558");        
+        if(book_sender!=nullptr)
+        {
+            pb::dms::BBO bbo_info;
+            fh::cme::market::message::Instrument instrument;
+            std::string symbol = "bbo_info_symbol_12345";
+            bbo_info.set_contract(symbol);
+            book_sender->OnBBO(bbo_info);
             delete book_sender;
             book_sender = nullptr;
         }
