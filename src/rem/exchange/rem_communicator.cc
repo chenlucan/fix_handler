8e4d3f914fefd6589c839f0f541fb5136c4d0e0b
王启超 <710265020@qq.com>
2017-05-23 11:49:36 +0800

    修改交易的数据鉴权接口

--- src/rem/exchange/rem_communicator.cc
+++ src/rem/exchange/rem_communicator.cc
@@ -894,8 +894,14 @@ void CRemGlobexCommunicator::Add(const ::pb::ems::Order& order)
        temp.m_Qty = order.quantity();
 	if(temp.LimitPrice <=0 || temp.Volume <=0)
 	 {
-            order.set_status(pb::ems::OrderStatus::OS_Rejected);	
-	     m_strategy->OnOrder(order);		
+	     ::pb::ems::Order tmporder;
+            tmporder.set_client_order_id(order.client_order_id());	
+            tmporder.set_account(order.account());
+            tmporder.set_contract(order.contract());	 
+            tmporder.set_status(pb::ems::OrderStatus::OS_Rejected);	
+	     m_strategy->OnOrder(tmporder);
+	     LOG_ERROR("Add Order Error");	 
+	     return;		
 	 }   
 //demo
 /*temp.m_Tif = EES_OrderTif_Day;
